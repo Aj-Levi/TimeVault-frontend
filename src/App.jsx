83470641e2886b39db5099b99eb301e6e8d9532a
Ajay <ajaykathar30gmail.com>
2025-11-08 12:08:20 +0530

    feat: implement user authentication with login and signup pages
    - Added Login and Signup components for user authentication.
    - Integrated axios for API calls to user authentication endpoints.
    - Created Redux slice for managing authentication state.
    - Configured Redux store and provided it to the application.
    - Updated App component to include routes for login and signup.
    - Added toast notifications for successful login and error handling.
    - Updated package.json to include necessary dependencies for Redux and toast notifications.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Home from "./pages/Home";
 import { EarthPage } from "./pages/earthPage/EarthPage";
 import EventPage from "./pages/EventPage";
 import SearchPage from "./pages/SearchPage";
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import toast, { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <>
+     <Toaster/>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="/globe" element={<EarthPage />} />
           <Route path="/search" element={<SearchPage/>}/>
           <Route path="/event/:id" element={<EventPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Route>
       </Routes>
     </>
